fd0fcf77af5251ab788da34d17ceb0a4c6efacab
Sangram Thapa <thapasangram64@gmail.com>
2025-01-10 14:11:05 +0545

    header

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./Header/Header"
-
+const heading={
+  note:"24x7 Technical Support",
+  phone:"01-4435890"
+};
 
 function App() {
   return (
-    <Header/>
+    <Header heading={heading}/>
   )
 }
 
--- website/src/Header/Header.jsx
+++ website/src/Header/Header.jsx
@@ -1,9 +1,54 @@
+/* eslint-disable react/prop-types */
+import { FaPhone } from "react-icons/fa6";
+import { FaHeadphonesAlt } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { TiSocialTwitter } from "react-icons/ti";
+import { FaLinkedinIn } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
+import "./header.css";
 
-
-function Header() {
+function Header(props) {
+  const { note, phone } = props.heading;
   return (
-    <div>Header</div>
-  )
+    <>
+      <div className="heading">
+        <div className="header">
+          <ul>
+            <li>
+              <FaHeadphonesAlt/>
+              {note}
+            </li>
+            <li>
+              <FaPhone />
+              {phone}
+            </li>
+          </ul>
+        </div>
+        <div className="social-medias">
+          <div>
+            <a href="#">
+              <FaFacebook  style={{color:'white'}}/>
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <TiSocialTwitter style={{color:'white'}} />
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <FaLinkedinIn style={{color:'white'}}/>
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <IoLogoInstagram style={{color:'white'}}/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
